542436c8fc7b2c7a9d33156d27c08b0f99d4f072
pAplakidis <aplakidisp@gmail.com>
2020-10-11 15:46:31 +0300

    added skeleton for cpu

--- src/ps1/cpu/cpu.cc
+++ src/ps1/cpu/cpu.cc
@@ -6,6 +6,13 @@ Cpu::Cpu(){
 
 // Loads ps1 rom into m_rom member variable
 void Cpu::load_rom(std::string rom_path){
+  std::vector<char> rom_data; // buffer for storing data from ROM
+
+  // TODO: open rom_file, read data to rom_data, copy the rom_data to m_memory in the correct offset (according to the docs)
+}
+
+// Reads command in memory and executes it (also increases pc to point to next instruction)
+void Cpu::cycle(){
   
 }
 
--- src/ps1/cpu/cpu.h
+++ src/ps1/cpu/cpu.h
@@ -3,13 +3,16 @@
 #include <inttypes.h>
 
 class Cpu{
-  //const uint32_t MEMORY_SIZE = 4096;    // CHANGE THIS 4KB instruction memory, 1KB data cache
 
 public:
-  std::vector<char> m_rom_data; // buffer for storing data from ROM
+  //const uint32_t MEMORY_SIZE = 4096;    // CHANGE THIS maybe?
+  static const uint32_t MEMORY_SIZE = 5120;    // 4KB instruction memory, 1KB data cache
+
+  unsigned char m_memory[MEMORY_SIZE];
 
   Cpu();
   void load_rom(std::string rom_path);
+  void cycle();
 
 };
 
