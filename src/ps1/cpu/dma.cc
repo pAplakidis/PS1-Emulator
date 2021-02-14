2365fb194862116e2568873f67743a2b8dac6404
pAplakidis <aplakidisp@gmail.com>
2021-02-14 13:24:50 +0200

    handled DMA interrupt register

--- src/ps1/cpu/dma.cc
+++ src/ps1/cpu/dma.cc
@@ -15,3 +15,37 @@ void Dma::set_control(uint32_t value){
   control = value;
 }
 
+// Return the status of the DMA interrupt
+bool Dma::irq(){
+  uint8_t channel_irq = channel_irq_flags & channel_irq_en;
+  return force_irq || (irq_en && channel_irq != 0);
+}
+
+// Retrieve the value of the interrupt register
+uint32_t Dma::interrupt(){
+  uint32_t r = 0;
+
+  r |= (uint32_t)irq_dummy;
+  r |= (uint32_t)force_irq << 15;
+  r |= (uint32_t)channel_irq_en << 16;
+  r |= (uint32_t)irq_en << 23;
+  r |= (uint32_t)channel_irq_flags << 24;
+  r |= (uint32_t)irq() << 31;
+
+  return r;
+}
+
+// Set the value of the interrupt register
+void Dma::set_interrupt(uint32_t value){
+  // Unknown what bits [5:0] do
+  irq_dummy = (uint8_t)(value & 0x3f);
+
+  force_irq = (value >> 15) & 1 != 0;
+  channel_irq_en = (uint8_t)((value >> 16) & 0x7f);
+  irq_en = (value >> 23) & 1 != 0;
+
+  // Writing 1 to a flag resets it
+  uint8_t ack = (uint8_t)((value >> 24) & 0x3f);
+  channel_irq_flags &= !ack;
+}
+
--- src/ps1/cpu/dma.h
+++ src/ps1/cpu/dma.h
@@ -7,9 +7,27 @@ private:
   // DMA control register
   uint32_t control;
 
+  // master IRQ enable
+  bool irq_en;
+
+  // IRQ enable for individual channels
+  uint8_t channel_irq_en;
+
+  // RIQ flags for individual channels
+  uint8_t channel_irq_flags;
+
+  // When set the interrupt is active unconditionally (even if 'irq_en' is false)
+  bool force_irq;
+
+  // Store bits [0:5] of the interrupt registers (RW) and send them back untouched on reads
+  uint8_t irq_dummy;
+
 public:
   Dma();
   uint32_t get_control();
   void set_control(uint32_t value);
+  bool irq();
+  uint32_t interrupt();
+  void set_interrupt(uint32_t value);
 };
 
